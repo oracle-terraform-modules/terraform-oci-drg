4c600d3ef6b07f9dc160d0713206902c82bdbe6f
Andrea Marchesini <andrea.marchesini@oracle.com>
2022-07-27 11:00:52 +0000

    fix: removed the region variable

--- variables.tf
+++ variables.tf
@@ -3,11 +3,7 @@
 
 # general oci parameters
 
-variable "region" {
-  # List of regions: https://docs.cloud.oracle.com/iaas/Content/General/Concepts/regions.htm#ServiceAvailabilityAcrossRegions
-  description = "The OCI region where the DRG will be created."
-  type        = string
-}
+
 variable "compartment_id" {
   description = "compartment id where to create all resources"
   type        = string
